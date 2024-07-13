70da33e54d1e2c111783ea53cd8031b62d44843b
Shougot mollik <shougotmollik@gmail.com>
2024-07-14 02:01:43 +0600

    appbar

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_ui/views/screens/homescreen/home_screen.dart';
+import 'package:whatsapp_ui/views/screens/mainNavbarScreen/main_navbar_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: MainNavbarScreen(),
     );
   }
 }
